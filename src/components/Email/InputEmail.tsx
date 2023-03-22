76ef1cb2731e2c691d7951d9e2e2b2a4ef6b23e1
kevin <kevindiegodasilvasousa@gmail.com>
2023-03-22 16:20:11 -0300

    feat: create input email component

---
+++ src/components/Email/InputEmail.tsx
@@ -0,0 +1,17 @@
+import './InputEmail.css'
+
+type PropsEmailValidate = {
+    labelText: string,
+    email: string,
+}
+
+export function EmailInput(props: PropsEmailValidate) {
+    return (
+        <>
+            <label htmlFor="email">
+                {props.labelText}
+            </label>
+            <input type="email" value={props.email} />
+        </>
+    );
+}
