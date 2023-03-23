ab5d1051781f416ddbdbb149e1046492d963a8c5
Bruno Mateus <bruno_205@hotmail.com>
2023-03-23 19:04:25 -0300

    Tela de login (#26)
    Co-authored-by: Bruno Mateus Oliveira gomes <Bruno-mateus>
    Co-authored-by: Guilherme Scarmagnani <66280834+guilhermeSDB@users.noreply.github.com>

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
-import { Home } from "../pages/home";
+import { Home } from "../pages/Home/home";
+
 
 export const router = createBrowserRouter([
   {
