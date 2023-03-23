ff9b1e535ec445e8a72ce41a421779c52be2a68c
Edu Nunes <edu.nunes.github@gmail.com>
2023-03-23 19:16:49 -0300

    Criação da página de novas contas (#18)
    Co-authored-by: LucasFell <lucas.fell@partner.bmw.com.br>
    Co-authored-by: Guilherme Scarmagnani <66280834+guilhermeSDB@users.noreply.github.com>

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
+import { CreateNewAccount } from "../pages/CreateNewAccount/CreateNewAccount";
 import { Home } from "../pages/Home/home";
-
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/new-account",
+    element: <CreateNewAccount />,
   },
 ]);
